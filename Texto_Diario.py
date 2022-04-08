690b13abfe3e01437affc2bbbdd0c20660eba88e
Caio Adami <caio_mazza@hotmail.com>
2022-04-08 19:29:19 -0300

    Removendo variáveis desnecessárias e criando funções

--- Texto_Diario.py
+++ Texto_Diario.py
@@ -3,23 +3,31 @@ from bs4 import BeautifulSoup
 from datetime import date
 from os import startfile
 
-url = 'https://wol.jw.org/pt/wol/h/r5/lp-t'
-gross_html = requests.get(url, headers={'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'})
-todays_date_for_div = f'{date.today().year}-{date.today().month:02}-{date.today().day:02}T00:00:00.000Z'
+def gather_info(url):
+    gross_html = requests.get(url, headers={'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'})
+    todays_date_for_div = f'{date.today().year}-{date.today().month:02}-{date.today().day:02}T00:00:00.000Z'
+    return gross_html, todays_date_for_div
 
-soup = BeautifulSoup(gross_html.text, 'lxml')
-html_today_div = soup.find('div', {'data-date': todays_date_for_div})
+def html_handling(gross_html, todays_date_for_div):
+    soup = BeautifulSoup(gross_html.text, 'lxml')
+    html_today_div = soup.find('div', {'data-date': todays_date_for_div})
+    return html_today_div
 
-soup_html_today_div = BeautifulSoup(str(html_today_div), 'lxml')
-html_today_div_comments = soup_html_today_div.find_all('p')[-1].text
+def txt_info(html_today_div):
+    txt_header = html_today_div.h2.text
+    txt_bible_versicle = html_today_div.p.text
+    txt_comments = html_today_div.find_all('p')[-1].text
+    return txt_header, txt_bible_versicle, txt_comments
 
-txt_header = soup_html_today_div.h2.text
-txt_bible_versicle = soup_html_today_div.p.text
-txt_comments = soup_html_today_div.find_all('p')[-1].text
+def writing_txt(txt_header, txt_bible_versicle, txt_comments):
+    with open(f'Texto Diário - {date.today().day:02}-{date.today().month:02}-{date.today().year}.txt', 'w') as file:
+        file.write(txt_header + '\n\n')
+        file.write(txt_bible_versicle + '\n\n')
+        file.write(txt_comments)
 
-with open(f'Texto Diário - {date.today().day:02}-{date.today().month:02}-{date.today().year}.txt', 'w') as file:
-    file.write(txt_header + '\n\n')
-    file.write(txt_bible_versicle + '\n\n')
-    file.write(txt_comments)
+html, current_date = gather_info('https://wol.jw.org/pt/wol/h/r5/lp-t')
+div = html_handling(html, current_date)
+header, bible_versicle, comments = txt_info(div)
+writing_txt(header, bible_versicle, comments)
 
 startfile(f'Texto Diário - {date.today().day:02}-{date.today().month:02}-{date.today().year}.txt')
